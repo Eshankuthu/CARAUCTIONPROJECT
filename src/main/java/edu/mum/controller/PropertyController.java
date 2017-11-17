e74e6359f595c29aa40edb678a4643a2ad841e83
eshankuthu <kuthu.eshan@gmail.com>
2017-11-17 12:41:35 -0600

    Modify property class

--- src/main/java/edu/mum/controller/PropertyController.java
+++ src/main/java/edu/mum/controller/PropertyController.java
@@ -1,5 +1,28 @@
 package edu.mum.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import edu.mum.domain.Property;
+import edu.mum.domain.User;
+
+@Controller
 public class PropertyController {
+	
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String addProperty(@ModelAttribute("newProperty") Property property, Model model) {
+		// property.setOwner(currentUser);
+		return "propertyForm";
+	}
+
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String addProperty(@ModelAttribute("newProperty") Property property, Model model, User currentUser) {
+		// property.setOwner(currentUser);
+		return "propertyForm";
+	}
 
 }
--- src/main/webapp/WEB-INF/jsp/welcome.jsp
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -1,3 +1,4 @@
 <%@ include file="header.jsp"%>
   <h1>Welcome to Home Page</h1>
+  
 <%@ include file="footer.jsp"%>
